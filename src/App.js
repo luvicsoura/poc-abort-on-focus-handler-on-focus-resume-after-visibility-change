b9ccc4869d21f91c55147fe374c9cad4d5644a45
Victor Oliveira <victoroliveira.dev@gmail.com>
2022-06-09 18:27:11 -0300

    feat: creat poc of onfocus prevention on visibility change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  visibilityChange = false;
+
+  state = {
+    focused: false,
+    prevented: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener('visibilitychange', this.onVisibilityChange);
+  }
+
+  onVisibilityChange = (event) => {
+    console.log('visibility change event', event);
+    this.visibilityChange = true;
+    setTimeout(() => this.visibilityChange = false, 3000);
+  };
+
+  onInputFocus = (event) => {
+    console.log('focus event', event);
+
+    if (this.visibilityChange) {
+      console.log('on focus prevented', this.visibilityChange);
+      this.visibilityChange = false;
+      this.setState({ prevented: true });
+      return;
+    }
+
+    this.setState({ prevented: false });
+    this.setState(
+      { focused: true },
+      () => {
+        setTimeout(() => this.setState({
+          focused: false 
+        }), 3000);
+      }
+    );
+  };
+
+  render() {
+
+    const appStyle = {
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: '#282c34',
+    };
+
+    const titleStyle = {
+      margin: 0,
+      padding: 24,
+      height: 40,
+      fontWeight: 700,
+    };
+
+    return(
+      <div style={appStyle}>
+        <input 
+          onFocus={this.onInputFocus}
+        />
+        <h1 style={titleStyle}>
+          {this.state.focused && (
+            <span style={{color: 'red'}}>From onFocus!!!</span>
+          )}
+          {this.state.prevented && (
+            <span style={{color: 'green'}}>onFocus prevented!!!</span>
+          )}
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
